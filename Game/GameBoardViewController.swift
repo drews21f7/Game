cd2c28b6b06014da67238698914725414f3f3d96
Leah <musicalshorty65@gmail.com>
2019-07-18 09:48:25 -0600

    updated comments

--- Game/GameBoardViewController.swift
+++ Game/GameBoardViewController.swift
@@ -13,30 +13,41 @@ class GameBoardViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet var buttons: [UIButton]!
     
+    var count = 1
+    var activePlayer = 1
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     var gameIsActive = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+//        if (gameState[sender.tag-1] == 0 && gameIsActive == true) {
+//            gameState[sender.tag-1] = activePlayer
+//            if (activePlayer == 1) {
+//                sender.setImage(UIImage(named:"X"), for: UIControl.State()) {
+//                    activePlayer = 2
+//                } else {
+//                    sender.setImage(UIImage(named: "O"), for: UIControl.State) {
+//                        activePlayer = 1
+//                    }
+//                }
 //
-//    @IBAction func buttonTapped(_ sender: Any) {
+//            }
 //
-//    }
-//
+//        }
+    }
     
     
+    @IBAction func playAgainButtonTapped(_ sender: Any) {
+        //Hopefully this works! I have no idea what is happening!!!! AAAHHH!!!!
+        
+    }
     
-//    func presentSimpleAlert(title: String) {
-//        let alertController = UIAlertAction(title: "Winner winner chicken Dinner", style: .default)
-//        let dismissAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
-//
-//    }
-
-
+    
 }
 
 
