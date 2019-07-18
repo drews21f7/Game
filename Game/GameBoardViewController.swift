5dcaa6b44893752351a238ef38997fbe548cf439
Leah <musicalshorty65@gmail.com>
2019-07-18 15:51:55 -0600

    polished project

--- Game/GameBoardViewController.swift
+++ Game/GameBoardViewController.swift
@@ -12,9 +12,11 @@ import SAConfettiView
 
 class GameBoardViewController: UIViewController {
     
+
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet weak var winLabel: UILabel!
+    
     var confetti: SAConfettiView?
     
     var count = 1
@@ -23,29 +25,32 @@ class GameBoardViewController: UIViewController {
     var gameIsActive = 1
     let winningCombinations: [[Int]] = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
- 
     override func viewDidLoad() {
         super.viewDidLoad()
         winLabel.text = ""
         // Do any additional setup after loading the view.
-        
     }
     
     func winner() {
-        let confettiView = SAConfettiView(frame: self.view.bounds)
+       let confettiView = SAConfettiView(frame: self.view.bounds)
+        confettiView.isUserInteractionEnabled = false
         self.view.addSubview(confettiView)
         self.confetti = confettiView
-        confettiView.type = .Diamond
-        confettiView.intensity = 0.5
-        confettiView.colors = [UIColor.black, UIColor.blue, UIColor.gray, UIColor.green, UIColor.purple, UIColor.yellow, UIColor.red]
+        confettiView.type = .Star
+        confettiView.intensity = 0.69
+        confettiView.colors = [UIColor.black, UIColor.blue, UIColor.gray, UIColor.green, UIColor.purple, UIColor.yellow]
         confettiView.startConfetti()
     }
     
     func stopConfetti() {
-        let confettiView = SAConfettiView(frame: self.view.bounds)
-        confettiView.stopConfetti()
+        
+        for view in view.subviews where view is SAConfettiView {
+            view.removeFromSuperview()
+        }
     }
     
+    
+
     @IBAction func buttonTapped(_ sender: AnyObject) {
         if gameState[sender.tag-1] == 0 && gameIsActive == 1 {
             gameState[sender.tag-1] = activePlayer
@@ -61,6 +66,7 @@ class GameBoardViewController: UIViewController {
                 print (gameState)
             }
         }
+    
         // Checks gameState array to see if a player has met a winning combination
         for combination in winningCombinations {
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
@@ -71,9 +77,7 @@ class GameBoardViewController: UIViewController {
                     print ("Cross wins")
                     winLabel.text = "CROSS WINS!"
                     winner()
-                    
-                    
-                    
+       
                     
                 } else {
                     gameState = [2, 2, 2, 2, 2, 2, 2, 2, 2]
@@ -106,13 +110,13 @@ class GameBoardViewController: UIViewController {
         }
     }
     
-    
     @IBAction func playAgainButtonTapped(_ sender: Any) {
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         activePlayer = 1
         gameIsActive = 1
         winLabel.text = ""
         stopConfetti()
+       
         
         // Resets button images back to nil
         for tag in 1...9 {
